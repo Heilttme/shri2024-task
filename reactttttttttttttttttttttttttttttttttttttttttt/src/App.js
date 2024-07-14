bf0c78bd82dc2c022a4d33fd7e8df5a833a74717
n.sorokin <n.sorokin@ivpa.ru>
2024-07-14 09:07:03 +0300

    BUI*D????????????

---
+++ reactttttttttttttttttttttttttttttttttttttttttt/src/App.js
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { Main } from "./Main";
+import "./reset.css";
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default App;
